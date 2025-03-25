19d7b73b9be5d15cabe4e7e19e156fbd15e17d50
Nisarg <nisargdadhania8@gmail.com>
2025-03-25 22:58:34 +0530

    made new changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -18,9 +18,7 @@ const Navbar = ({ setShowLogin }) => {
   }
   return (
     <div className="navbar">
-      <Link to="/">
-        <img src={assets.logo} alt="" className="logo" />
-      </Link>
+  
       <ul className="navbar-menu">
         <Link
           to="/"
